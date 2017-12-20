cde600d0ad38d5f749a80f97a80e5b4065fea4b2
diegopal <dieropal@espol.edu.ec>
2017-12-20 17:35:49 -0500

    Using bonuspack for routing
    * Added "osmbonuspack" for helping to get route information from osrm server
    * Road drawn properly on the map

--- app/src/main/java/com/example/geotaxi/geotaxi/MainActivity.kt
+++ app/src/main/java/com/example/geotaxi/geotaxi/MainActivity.kt
@@ -18,19 +18,12 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.content.Intent
-import android.graphics.Color
+import android.os.AsyncTask
 import android.provider.Settings
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import org.json.JSONObject
+import org.osmdroid.bonuspack.routing.OSRMRoadManager
+import org.osmdroid.bonuspack.routing.Road
+import org.osmdroid.bonuspack.routing.RoadManager
 import org.osmdroid.views.overlay.Marker
-import org.osmdroid.views.overlay.Polyline
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import java.io.IOException
 import kotlin.collections.ArrayList
 
 
@@ -38,11 +31,11 @@ class MainActivity : AppCompatActivity() {
     val MIN_TIME: Long = 10000
     val MIN_DISTANCE: Float = 10F
     val MY_PERMISSIONS_REQUEST_LOCATION = 1
+    val ROUTE_SERVICE_URL = "http://192.168.0.115:5000/route/v1/car/"
     val locationListener = MyLocationListener()
-    val startGp = GeoPoint(-2.2284,-79.8990)
-    val endGp = GeoPoint(-2.1478,-79.9655)
+    val startGp = GeoPoint(-2.17825,-79.82591)
+    val endGp = GeoPoint(-2.14045,-79.86407)
     var map: MapView? = null
-    var osrmAPI: OsrmAPI? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,7 +50,9 @@ class MainActivity : AppCompatActivity() {
         map?.setTileSource(TileSourceFactory.MAPNIK)
         map?.setBuiltInZoomControls(true)
         map?.setMultiTouchControls(true)
-
+        val mapController = map?.controller
+        mapController?.setCenter(startGp)
+        mapController?.setZoom(17)
         // check access location permission
         if (ContextCompat.checkSelfPermission(this,
 
@@ -79,8 +74,8 @@ class MainActivity : AppCompatActivity() {
                         arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                         MY_PERMISSIONS_REQUEST_LOCATION)
             }
-        } else {
 
+        } else {
 
             val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
             val isGPSEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
@@ -92,39 +87,16 @@ class MainActivity : AppCompatActivity() {
                 Log.d("activity","gps not enable")
                 enableLocationSettings()
             }
-            val mapController = map?.controller
+
             val startPoint = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
             if (startPoint !== null) {
                 val currentLocation = GeoPoint(startPoint)
-                mapController?.setCenter(startGp)
-                mapController?.setZoom(17)
             }
 
-            getRoute()
+            //get and draw road on the map
+            executeRoadTask()
         }
 
-    }
-
-    fun getRoute() {
-        val client = OkHttpClient.Builder()
-                .addInterceptor(MyInterceptor())
-                .build()
-
-        //route request to orsm server
-        val retrofit = Retrofit.Builder()
-                .baseUrl("http://router.project-osrm.org/")
-                .addConverterFactory(ScalarsConverterFactory.create())
-                .client(client)
-                .build()
-        osrmAPI = retrofit.create(OsrmAPI::class.java)
-
-        val osrmCall = osrmAPI?.getRoute(
-                startGp.longitude.toString(),
-                startGp.latitude.toString(),
-                endGp.longitude.toString(),
-                endGp.latitude.toString())
-
-        osrmCall?.enqueue(MyCallback())
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
@@ -151,7 +123,8 @@ class MainActivity : AppCompatActivity() {
                             enableLocationSettings()
                         }
 
-                        getRoute()
+                        //get and draw road on the map
+                        executeRoadTask()
                     }
 
                 } else {
@@ -168,6 +141,25 @@ class MainActivity : AppCompatActivity() {
     private fun enableLocationSettings() {
         val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
         startActivity(settingsIntent)
+    }
+
+    private  fun executeRoadTask(){
+        val roadTask = RoadTask()
+        roadTask.execute(applicationContext)
+    }
+    
+    private fun drawRoad(road: Road) {
+        var roadOverlay = RoadManager.buildRoadOverlay(road)
+        map?.overlays?.add(roadOverlay)
+        val startMarker = Marker(map)
+        startMarker.setPosition(startGp)
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        map?.getOverlays()?.add(startMarker)
+
+        val endMarker = Marker(map)
+        endMarker.setPosition(endGp)
+        endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        map?.getOverlays()?.add(endMarker)
     }
 
     inner class MyLocationListener : LocationListener {
@@ -189,74 +181,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    inner class MyCallback : Callback<String> {
-
-        override fun onResponse(call: Call<String>?, response: Response<String>?) {
-            drawBestRoute(response)
+    inner class RoadTask : AsyncTask<Context, Void, Road>() {
+        override fun doInBackground(vararg params: Context?): Road {
+            val roadManager = OSRMRoadManager(params[0])
+            val wayPoints = ArrayList<GeoPoint>(0)
+            wayPoints.add(startGp)
+            wayPoints.add(endGp)
+            roadManager.setService(ROUTE_SERVICE_URL)
+            val road = roadManager.getRoad(wayPoints)
+            return road
         }
 
-        override fun onFailure(call: Call<String>?, t: Throwable) {
-
-            Log.d("activity",String.format("Failure %s",t.message))
-        }
-    }
-
-    private fun drawBestRoute(response: Response<String>?) {
-        val startMarker = Marker(map)
-        startMarker.setPosition(startGp)
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        map?.getOverlays()?.add(startMarker)
-
-        val endMarker = Marker(map)
-        endMarker.setPosition(endGp)
-        endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        map?.getOverlays()?.add(endMarker)
-
-        var line = Polyline()
-        line.width = 10f
-        line.color = Color.argb(90, 0,0,254)
-        line.setGeodesic(true)
-
-        var path = ArrayList<GeoPoint>(0)
-        val root = JSONObject(response?.body())
-        val routes = root.getJSONArray("routes")
-        val route = routes.getJSONObject(0)
-        val legs = route.getJSONArray("legs")
-        val leg = legs.getJSONObject(0)
-        val steps = leg.getJSONArray("steps")
-        for (i in 0..(steps.length()-1)){
-            val step = steps.getJSONObject(i)
-            val intersections = step.getJSONArray("intersections")
-            for (j in 0..(intersections.length()-1)){
-                val intersection = intersections.getJSONObject(j)
-                val location = intersection.getJSONArray("location")
-                val longitude = location[0] as Double
-                val latitude = location[1] as Double
-                path.add(GeoPoint(latitude, longitude))
-
-            }
-        }
-
-        line.points = path
-        map?.overlays?.add(line)
-    }
-
-    inner class MyInterceptor : Interceptor {
-        @Throws(IOException::class)
-        override fun intercept(chain: Interceptor.Chain): okhttp3.Response? {
-            val request = chain.request()
-
-            val t1 = System.nanoTime()
-            Log.d("activity",String.format("Sending request %s on %s%n%s",
-                    request.url(), chain.connection(), request.headers()))
-
-            val response = chain.proceed(request)
-
-            val t2 = System.nanoTime()
-            Log.d("activity", String.format("Received response for %s in %.1fms%n%s",
-                    response.request().url(), (t2 - t1) / 1e6, response.headers()))
-
-            return response
+        override fun onPostExecute(result: Road) {
+            super.onPostExecute(result)
+            drawRoad(result)
         }
     }
 }
