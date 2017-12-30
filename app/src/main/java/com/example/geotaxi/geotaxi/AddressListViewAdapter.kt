26073c20328d4f92c66a72cb5251c34aca59a349
diegopal <dieropal@espol.edu.ec>
2017-12-30 00:58:46 -0500

    Using Nominatim server for geocoder service
    * Added an edit text view  for let user search for a location
    * Added recycler view for show the list of addresses found from a specific address name
    * Get and draw a road from current location to destination location that user choosed

---
+++ app/src/main/java/com/example/geotaxi/geotaxi/AddressListViewAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.geotaxi.geotaxi
+
+import android.location.Address
+import android.support.v7.widget.CardView
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class AddressListViewAdapter(dataset: List<Address>, rVClickListener: View.OnClickListener) : RecyclerView.Adapter<AddressListViewAdapter.Companion.ViewHolder>() {
+
+    var mDataset = dataset
+    val mClickListener = rVClickListener
+
+    // Provide a reference to the views for each data item
+    // you provide access to all the views for a data item in a view holder
+    companion object {
+         class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+             val mLayoutView = v
+
+             fun setAddress(address: String) {
+                 mLayoutView.findViewById<TextView>(R.id.address_tv).text = address
+             }
+
+             fun setLocation(location: String) {
+                 mLayoutView.findViewById<TextView>(R.id.location_tv).text = location
+             }
+
+             fun setClickListener(cl: View.OnClickListener) {
+                 val cardView = mLayoutView.findViewById<CardView>(R.id.address_item_card_view)
+                 cardView.setOnClickListener(cl)
+             }
+         }
+    }
+
+    override fun getItemCount(): Int {
+        return mDataset.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        // create a new view
+        val v = LayoutInflater.from(parent?.context)
+                .inflate(R.layout.address_item_list, parent, false) as View
+
+        return ViewHolder(v)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        val address = mDataset[position].extras["display_name"].toString()
+        val location = mDataset[position].latitude.toString() + ", " +
+                             mDataset[position].longitude.toString()
+        holder?.setAddress(address)
+        holder?.setLocation(location)
+        holder?.setClickListener(mClickListener)
+    }
+
+}
--- app/src/main/java/com/example/geotaxi/geotaxi/MainActivity.kt
+++ app/src/main/java/com/example/geotaxi/geotaxi/MainActivity.kt
@@ -18,37 +18,62 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.content.Intent
+import android.location.Address
 import android.os.AsyncTask
 import android.provider.Settings
+import android.support.v7.widget.CardView
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.KeyEvent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.osmdroid.bonuspack.location.GeocoderNominatim
 import org.osmdroid.bonuspack.routing.OSRMRoadManager
 import org.osmdroid.bonuspack.routing.Road
 import org.osmdroid.bonuspack.routing.RoadManager
 import org.osmdroid.views.overlay.Marker
+import java.util.*
 import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
-    val MIN_TIME: Long = 10000
+    val MIN_TIME: Long = 5000
     val MIN_DISTANCE: Float = 10F
     val MY_PERMISSIONS_REQUEST_LOCATION = 1
-    val ROUTE_SERVICE_URL = "http://192.168.0.115:5000/route/v1/car/"
+    val ROUTE_SERVICE_URL = "http://192.168.0.107:5000/route/v1/car/"
+    val NOMINATIM_SERVICE_URL = "http://192.168.0.107:80/nominatim/"
     val locationListener = MyLocationListener()
-    val startGp = GeoPoint(-2.17825,-79.82591)
-    val endGp = GeoPoint(-2.14045,-79.86407)
+    var startGp: GeoPoint = GeoPoint(-2.1811931,-79.8765573)//Guayaquil
+    var endGp: GeoPoint? = null
+    var locationName = ""
     var map: MapView? = null
-
+    var addressRecyclerView: RecyclerView? = null
+    var addressCardView: CardView? = null
+    var taxi_request: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val ctx = applicationContext
-        //important! set your user agent to prevent getting banned from the osm servers
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
-
         setContentView(R.layout.activity_main)
+
+        val searchEV = findViewById<EditText>(R.id.search)
+        searchEV.setImeActionLabel("Buscar", KeyEvent.KEYCODE_ENTER)
+        searchEV.setOnEditorActionListener(MyEditionActionListener())
+        addressCardView = findViewById<CardView>(R.id.address_card_view)
+        addressRecyclerView = findViewById<RecyclerView>(R.id.address_recycler_view)
+        taxi_request = findViewById<Button>(R.id.taxi_request_button)
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        addressRecyclerView?.setHasFixedSize(true)
+        // use a linear layout manager
+        val mLayoutManager = LinearLayoutManager(this)
+        addressRecyclerView?.setLayoutManager(mLayoutManager)
+
         map = findViewById<View>(R.id.map) as MapView
         map?.setTileSource(TileSourceFactory.MAPNIK)
-        map?.setBuiltInZoomControls(true)
         map?.setMultiTouchControls(true)
         val mapController = map?.controller
         mapController?.setCenter(startGp)
@@ -81,7 +106,7 @@ class MainActivity : AppCompatActivity() {
             val isGPSEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
             if (isGPSEnable) {
                 //Request location updates:
-                //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener)
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener)
             } else {
 
                 Log.d("activity","gps not enable")
@@ -90,11 +115,9 @@ class MainActivity : AppCompatActivity() {
 
             val startPoint = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
             if (startPoint !== null) {
-                val currentLocation = GeoPoint(startPoint)
+                startGp = GeoPoint(startPoint)
+                mapController?.setCenter(startGp)
             }
-
-            //get and draw road on the map
-            executeRoadTask()
         }
 
     }
@@ -114,17 +137,13 @@ class MainActivity : AppCompatActivity() {
                         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
                         val isGPSEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
                         if (isGPSEnable) {
-
                             //Request location updates:
-                            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener)
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener)
                         } else {
 
                             Log.d("activity","gps not enable")
                             enableLocationSettings()
                         }
-
-                        //get and draw road on the map
-                        executeRoadTask()
                     }
 
                 } else {
@@ -138,39 +157,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Go to the device settings for let user enable gps location
     private fun enableLocationSettings() {
         val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
         startActivity(settingsIntent)
     }
 
-    private  fun executeRoadTask(){
-        val roadTask = RoadTask()
-        roadTask.execute(applicationContext)
-    }
-    
-    private fun drawRoad(road: Road) {
-        var roadOverlay = RoadManager.buildRoadOverlay(road)
-        map?.overlays?.add(roadOverlay)
-        val startMarker = Marker(map)
-        startMarker.setPosition(startGp)
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        map?.getOverlays()?.add(startMarker)
-
-        val endMarker = Marker(map)
-        endMarker.setPosition(endGp)
-        endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        map?.getOverlays()?.add(endMarker)
-    }
-
+    //Class that handle location changes
     inner class MyLocationListener : LocationListener {
 
         override fun onLocationChanged(location: Location) {
             val currentLocation = GeoPoint(location)
-            val mapController = map?.controller
-
-                mapController?.setCenter(currentLocation)
-            mapController?.setZoom(17)
-
+            startGp = currentLocation
         }
 
         override fun onProviderDisabled(provider: String) {}
@@ -181,20 +179,114 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    inner class RoadTask : AsyncTask<Context, Void, Road>() {
-        override fun doInBackground(vararg params: Context?): Road {
-            val roadManager = OSRMRoadManager(params[0])
-            val wayPoints = ArrayList<GeoPoint>(0)
-            wayPoints.add(startGp)
-            wayPoints.add(endGp)
-            roadManager.setService(ROUTE_SERVICE_URL)
-            val road = roadManager.getRoad(wayPoints)
-            return road
-        }
+    //Class that handle user input on address search
+    inner class MyEditionActionListener : TextView.OnEditorActionListener {
+        override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+            if ((event?.keyCode == KeyEvent.KEYCODE_ENTER)) {
+                val searchEV = v as EditText
+                locationName = searchEV.text.toString() + startGp.latitude +
+                        ", " + startGp.longitude
 
-        override fun onPostExecute(result: Road) {
-            super.onPostExecute(result)
-            drawRoad(result)
+                if (locationName == "") return false
+                executeFromLocationNameTask()
+                return true
+            }
+            return false
         }
     }
+
+    private fun executeFromLocationNameTask() {
+        val flnTask = FromLocationNameTask()
+        flnTask.execute()
+    }
+
+    //Class that use geocoder Nominatim server in async mode for get location from address name
+    inner class FromLocationNameTask : AsyncTask<Context, Void, List<Address>>() {
+        override fun doInBackground(vararg params: Context?): List<Address> {
+            val geoNominatim = GeocoderNominatim(Locale.getDefault(), System.getProperty("http.agent"))
+            //uncomment for use own server
+            //geoNominatim.setService(NOMINATIM_SERVICE_URL)
+            return geoNominatim.getFromLocationName(locationName,10)
+        }
+
+        override fun onPostExecute(result: List<Address>) {
+            super.onPostExecute(result)
+            if (result.size > 0) {
+                endGp = GeoPoint(result.get(0).latitude, result.get(0).longitude)
+                // specify an adapter
+                val mAdapter = AddressListViewAdapter(result, MyRVClickListener())
+                addressRecyclerView?.setAdapter(mAdapter)
+                addressCardView?.visibility = View.VISIBLE
+                taxi_request?.visibility = View.GONE
+
+            } else {
+                Log.d("activity","fail to match location name")
+            }
+        }
+    }
+
+    //Class that handle click action on an item of the list view of addresses
+    inner class MyRVClickListener : View.OnClickListener {
+
+        override fun onClick(v: View?) {
+            val geoPointStr = v?.findViewById<TextView>(R.id.location_tv)?.text.toString()
+            var geoPointList = geoPointStr.split(",").map { it.trim() }
+            endGp = GeoPoint((geoPointList[0]).toDouble(), geoPointList[1].toDouble())
+            addressCardView?.visibility = View.GONE
+            map?.overlays?.clear()
+            //calculate and draw road on map
+            executeRoadTask()
+        }
+
+    }
+
+    private  fun executeRoadTask(){
+        val roadTask = RoadTask()
+        roadTask.execute(applicationContext)
+    }
+
+    //Class that use OSRM server in async mode for get a Road, from start to destination point
+    inner class RoadTask : AsyncTask<Context, Void, Road>() {
+
+        override fun doInBackground(vararg params: Context?): Road? {
+            val roadManager = OSRMRoadManager(params[0])
+            val wayPoints = ArrayList<GeoPoint>(0)
+            if (endGp != null) {
+                wayPoints.add(startGp)
+                wayPoints.add(endGp as GeoPoint)
+                //uncomment for use own server
+                //roadManager.setService(ROUTE_SERVICE_URL)
+                val road = roadManager.getRoad(wayPoints)
+                return road
+            }
+
+            return null
+        }
+
+        override fun onPostExecute(result: Road?) {
+            super.onPostExecute(result)
+            if (result != null) {
+                drawRoad(result)
+                taxi_request?.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun drawRoad(road: Road) {
+        var roadOverlay = RoadManager.buildRoadOverlay(road)
+        map?.overlays?.add(roadOverlay)
+
+        val startMarker = Marker(map)
+        startMarker.setPosition(startGp)
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        map?.getOverlays()?.add(startMarker)
+
+        val endMarker = Marker(map)
+        endMarker.setPosition(endGp)
+        endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        map?.getOverlays()?.add(endMarker)
+
+        map?.controller?.setCenter(startGp)
+    }
+
 }
