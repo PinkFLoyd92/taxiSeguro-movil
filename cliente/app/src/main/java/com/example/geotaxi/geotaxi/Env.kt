973b009b4d499382948010b3de473c736e6b6816
diegopal <dieropal@espol.edu.ec>
2018-01-11 12:00:47 -0500

    Requesting location with google play services location

--- cliente/app/src/main/java/com/example/geotaxi/geotaxi/Env.kt
+++ cliente/app/src/main/java/com/example/geotaxi/geotaxi/Env.kt
@@ -4,6 +4,7 @@ class Env {
     companion object {
         const val OSRM_SERVER_URL = "http://192.168.0.107:5000/route/v1/car/"
         const val NOMINATIM_SERVER_URL = "http://192.168.0.107:80/nominatim/"
-        const val API_BASE_URL = "http://192.168.0.111:4000/v1/"
+        const val API_BASE_URL = "http://172.20.133.213:4000/v1/"
+        const val API_SOCKET_URL = "http://172.20.133.213:9000"
     }
 }
--- cliente/app/src/main/java/com/example/geotaxi/geotaxi/MainActivity.kt
+++ cliente/app/src/main/java/com/example/geotaxi/geotaxi/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.geotaxi.geotaxi
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
@@ -11,16 +13,14 @@ import android.preference.PreferenceManager
 import android.view.View
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
-import android.location.LocationManager
 import android.location.Location
-import android.location.LocationListener
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
 import android.content.Intent
+import android.content.IntentSender
 import android.location.Address
 import android.os.AsyncTask
-import android.provider.Settings
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.CardView
 import android.support.v7.widget.LinearLayoutManager
@@ -29,8 +29,12 @@ import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.*
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
+import io.socket.client.IO
+import io.socket.client.Socket
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import org.osmdroid.bonuspack.location.GeocoderNominatim
@@ -49,11 +53,12 @@ import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
-    val MIN_TIME: Long = 5000
-    val MIN_DISTANCE: Float = 10F
+    val LOCATION_REQUEST_INTERVAL: Long = 7000
+    val LR_FASTEST_INTERVAL: Long = 5000
+    val LR_PRIORITY = LocationRequest.PRIORITY_HIGH_ACCURACY
     val MY_PERMISSIONS_REQUEST_LOCATION = 1
-    val locationListener = MyLocationListener()
-    var startGp: GeoPoint = GeoPoint(-2.1811931,-79.8765573)//Guayaquil
+    val REQUEST_CHECK_SETTINGS = 1
+    var mCurrentLocation: GeoPoint = GeoPoint(-2.1811931,-79.8765573)//Guayaquil
     var endGp: GeoPoint? = null
     var locationName = ""
     var map: MapView? = null
@@ -63,6 +68,11 @@ class MainActivity : AppCompatActivity() {
     var serverAPI: ServerAPI? = null
     var currentRoad: Road? = null
     var clientId: String? = null
+    val socket = IO.socket(Env.API_SOCKET_URL)
+    var routeId: String? = ""
+    var mFusedLocationClient: FusedLocationProviderClient? = null
+    var mLocationRequest: LocationRequest? = null
+    var mLocationCallback: LocationCallback? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -77,7 +87,7 @@ class MainActivity : AppCompatActivity() {
         addressCardView = findViewById<CardView>(R.id.address_card_view)
         addressRecyclerView = findViewById<RecyclerView>(R.id.address_recycler_view)
         taxi_request = findViewById<Button>(R.id.taxi_request_button)
-        taxi_request?.setOnClickListener { getTaxi() }
+        taxi_request?.setOnClickListener { requestTaxi() }
         val search = findViewById<EditText>(R.id.search)
 
 
@@ -92,7 +102,7 @@ class MainActivity : AppCompatActivity() {
         map?.setTileSource(TileSourceFactory.MAPNIK)
         map?.setMultiTouchControls(true)
         val mapController = map?.controller
-        mapController?.setCenter(startGp)
+        mapController?.setCenter(mCurrentLocation)
         mapController?.setZoom(17)
 
         var mIntent = intent
@@ -100,8 +110,8 @@ class MainActivity : AppCompatActivity() {
 
         val geocoderBtn = findViewById<ImageButton>(R.id.geocoder_btn)
         geocoderBtn.setOnClickListener{
-            locationName = search.text.toString() + startGp.latitude +
-                    ", " + startGp.longitude
+            locationName = search.text.toString() + mCurrentLocation.latitude +
+                    ", " + mCurrentLocation.longitude
 
             if (locationName !== "")
                 executeFromLocationNameTask()
@@ -109,6 +119,8 @@ class MainActivity : AppCompatActivity() {
 
         // check access location permission
         try {
+
+            socketConnect()
             if (ContextCompat.checkSelfPermission(this,
 
                     Manifest.permission.ACCESS_FINE_LOCATION)
@@ -131,24 +143,9 @@ class MainActivity : AppCompatActivity() {
                 }
 
             } else {
-
-                val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-                val isGPSEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-                if (isGPSEnable) {
-                    //Request location updates:
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener)
-                } else {
-
-                    Log.d("activity","gps not enable")
-                    enableLocationSettings()
-                }
-
-                val startPoint = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                if (startPoint !== null) {
-                    startGp = GeoPoint(startPoint)
-                    mapController?.setCenter(startGp)
-                }
+                initLocationrequest()
             }
+
         } catch (e: Exception) {
             Log.d("activity",String.format("exception on main activity: %s ", e.message))
         }
@@ -168,16 +165,7 @@ class MainActivity : AppCompatActivity() {
                     if (ContextCompat.checkSelfPermission(this,
                             Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
-                        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-                        val isGPSEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-                        if (isGPSEnable) {
-                            //Request location updates:
-                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, locationListener)
-                        } else {
-
-                            Log.d("activity","gps not enable")
-                            enableLocationSettings()
-                        }
+                        initLocationrequest()
                     }
 
                 } else {
@@ -191,6 +179,115 @@ class MainActivity : AppCompatActivity() {
                 return
             }
         }
+    }
+
+    private fun initLocationrequest() {
+        createLocationRequest()
+        //get current location settings
+        val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest!!)
+        //check whether the current location settings are satisfied
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+        //Prompt the User to Change Location Settings
+        task.addOnSuccessListener(this) {
+            // All location settings are satisfied. The client can initialize
+            // location requests here.
+            // ...
+            locationRequest()
+        }
+        task.addOnFailureListener(this) { p0 ->
+            if (p0 is ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+
+                    p0.startResolutionForResult(this@MainActivity,
+                            REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException ) {
+                    Log.d("activity",String.format("exception on start resolution for results: %s ", sendEx.message))
+                }
+            }
+        }
+
+        mLocationCallback = object: LocationCallback(){
+
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    onLocationChanged(location)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        // Check which request we're responding to
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            // Make sure the request was successful
+            if (resultCode == Activity.RESULT_OK) {
+                // The user picked a contact.
+                // The Intent's data Uri identifies which contact was selected.
+                locationRequest()
+            }
+        }
+    }
+    private  fun createLocationRequest() {
+        mLocationRequest = LocationRequest()
+        mLocationRequest?.interval = LOCATION_REQUEST_INTERVAL
+        mLocationRequest?.fastestInterval = LR_FASTEST_INTERVAL
+        mLocationRequest?.priority = LR_PRIORITY
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun locationRequest() {
+        try {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            startLocationUpdates()
+            /*mFusedLocationClient?.lastLocation
+                    ?.addOnSuccessListener(this) { location ->
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            onLocationChanged(location)
+                        }
+                    }*/
+        } catch (e: Exception) {
+            Log.d("activity",String.format("exception on location request: %s ", e.message))
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        try {
+
+
+            mFusedLocationClient?.requestLocationUpdates(mLocationRequest,
+                    mLocationCallback,
+                    null /* Looper */)
+            Log.d("activity",String.format("start updates"))
+        } catch (e: Exception) {
+            Log.d("activity",String.format("exception when start location updates: %s ", e.message))
+        }
+    }
+
+    private fun socketConnect() {
+        try {
+            socket.on(Socket.EVENT_CONNECT) {
+                val userInfo = JsonObject()
+                userInfo.addProperty("_id", clientId)
+                userInfo.addProperty("role", "client")
+                socket.emit("SENDINFO", userInfo)
+            }.on("DANGER") { TODO()
+            }.on(Socket.EVENT_DISCONNECT) {
+                TODO()
+            }
+            socket.connect()
+        } catch (e: Exception) {
+            Log.d("activity",String.format("exception on socket: %s ", e.message))
+
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -219,26 +316,22 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    //Go to the device settings for let user enable gps location
-    private fun enableLocationSettings() {
-        val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-        startActivity(settingsIntent)
-    }
-
-    //Class that handle location changes
-    inner class MyLocationListener : LocationListener {
-
-        override fun onLocationChanged(location: Location) {
-            val currentLocation = GeoPoint(location)
-            startGp = currentLocation
+    private fun onLocationChanged(location: Location) {
+        val currentLocation = GeoPoint(location)
+        val data = JsonObject()
+        val pos = JsonObject()
+        mCurrentLocation = currentLocation
+        val mapController = map?.controller
+        mapController?.setCenter(currentLocation)
+        pos.addProperty("longitude", currentLocation.longitude)
+        pos.addProperty("latitude", currentLocation.latitude)
+        data.add("position", pos)
+        data.addProperty("route_id", routeId)
+        try {
+            socket.emit("POSITION", data)
+        } catch (e: Exception) {
+            Log.d("activity",String.format("exception on location change: %s ", e.message))
         }
-
-        override fun onProviderDisabled(provider: String) {}
-
-        override fun onProviderEnabled(provider: String) {}
-
-        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-
     }
 
     //Class that handle user input on address search
@@ -246,8 +339,8 @@ class MainActivity : AppCompatActivity() {
         override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
             if ((event?.keyCode == KeyEvent.KEYCODE_ENTER)) {
                 val searchEV = v as EditText
-                locationName = searchEV.text.toString() + startGp.latitude +
-                        ", " + startGp.longitude
+                locationName = searchEV.text.toString() + mCurrentLocation.latitude +
+                        ", " + mCurrentLocation.longitude
 
                 if (locationName == "") return false
                 executeFromLocationNameTask()
@@ -314,7 +407,7 @@ class MainActivity : AppCompatActivity() {
             val roadManager = OSRMRoadManager(params[0])
             val wayPoints = ArrayList<GeoPoint>(0)
             if (endGp != null) {
-                wayPoints.add(startGp)
+                wayPoints.add(mCurrentLocation)
                 wayPoints.add(endGp as GeoPoint)
                 //uncomment for use own server
                 //roadManager.setService(Env.OSRM_SERVER_URL)
@@ -340,7 +433,7 @@ class MainActivity : AppCompatActivity() {
         map?.overlays?.add(roadOverlay)
 
         val startMarker = Marker(map)
-        startMarker.setPosition(startGp)
+        startMarker.setPosition(mCurrentLocation)
         startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
         map?.getOverlays()?.add(startMarker)
 
@@ -349,10 +442,10 @@ class MainActivity : AppCompatActivity() {
         endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
         map?.getOverlays()?.add(endMarker)
 
-        map?.controller?.setCenter(startGp)
+        map?.controller?.setCenter(mCurrentLocation)
     }
 
-    private fun getTaxi() {
+    private fun requestTaxi() {
         val client = OkHttpClient.Builder()
                 .addInterceptor(MyInterceptor())
                 .build()
@@ -377,13 +470,15 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<JsonObject>?, response: Response<JsonObject>?) {
                Log.d("server response", String.format("Server response %s",
                        response.toString()))
-                if (response?.code()!! >= 200) {
-                    Toast.makeText(applicationContext, "Server response OK", Toast.LENGTH_SHORT).show()
-
+                if (response?.code()!! >= 200 && response?.code()!! < 210) {
+                    try {
+                        routeId = response.body()?.get("_id")?.asString
+                        //Log.d("activity",String.format("id response %s ", clientId))
+                        Toast.makeText(applicationContext, "Server response OK", Toast.LENGTH_SHORT).show()
+                    } catch (e: Exception) {
+                        Log.d("activity",String.format("exception on login: %s ", e.message))
+                    }
                 }
-
-
-
             }
 
         })
@@ -397,8 +492,8 @@ class MainActivity : AppCompatActivity() {
         val end = JsonObject()
         val coorStart = JsonArray()
         val coorEnd = JsonArray()
-        coorStart.add(startGp.longitude)
-        coorStart.add(startGp.latitude)
+        coorStart.add(mCurrentLocation.longitude)
+        coorStart.add(mCurrentLocation.latitude)
         coorEnd.add(endGp?.longitude)
         coorEnd.add(endGp?.longitude)
         start.addProperty("type", "Point")
